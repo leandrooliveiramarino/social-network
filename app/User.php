7e2a7ce86f8e770bf543e910cb6402f4ef90cd02
= <=>
2016-05-29 18:07:59 -0300

    Habilitar as opções cadastrar e entrar

--- app/User.php
+++ app/User.php
@@ -2,25 +2,18 @@
 
 namespace App;
 
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Model;
 
-class User extends Authenticatable
+class User extends Model implements Authenticatable
 {
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+	use \Illuminate\Auth\Authenticatable;
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+	public function posts() {
+		return $this->hasMany('App\Post');
+	}
+
+    public function likes() {
+    	return $this->hasMany('App\Like');
+    }
 }
